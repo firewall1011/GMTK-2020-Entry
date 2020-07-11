fa77e88c8149dccc660c4e40552dd67d2d11d18b
Tiago <tmsilva11@gmail.com>
2020-07-11 19:46:41 -0300

    Add: shape shift material and properties change

---
+++ Assets/Scripts/ShapeMaterialSO.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class ShapeMaterialSO : ScriptableObject
+{
+    [Header("Physics properties")]
+    [Range(0.0000001f, 1000000000f)]
+    public float mass;
+    public PhysicMaterial physicMaterial;
+
+    [Header("Jump properties")]
+    public float JumpForce;
+    public float MaxDistance;
+    public float Radius;
+    public PlayerJump.GroundDetectionMode GroundDetection = 0;
+
+}
