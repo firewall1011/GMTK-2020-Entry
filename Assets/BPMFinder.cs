4678dfd5034067e42592555a8a45e1136c517333
mschiffhauer <mjs7593@psu.edu>
2020-07-12 00:58:54 -0400

    Initial

---
+++ Assets/BPMFinder.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BPMFinder : MonoBehaviour
+{
+    public float bpm;
+    private float beatInterval, beatTimer, beatIntervalEighth, beatTimerEighth;
+    public static bool beatFull, beatEighth;
+    public static int beatCount, beatCountEighth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void detectBeat()
+    {
+        //counts full beats
+        beatFull = false;
+        beatInterval = 60 / bpm;
+        beatTimer += Time.deltaTime;
+
+        if(beatTimer >= beatInterval)
+        {
+            beatTimer -= beatInterval;
+            beatCount++;
+            beatFull = true;
+            Debug.Log("Full");
+        }
+
+        // 1/8 beat count
+        beatEighth = false;
+        beatIntervalEighth = beatInterval / 8;
+        beatTimerEighth += Time.deltaTime;
+
+        if(beatTimerEighth >= beatIntervalEighth)
+        {
+            beatTimerEighth -= beatIntervalEighth;
+            beatEighth = true;
+            beatCountEighth++;
+            Debug.Log("D8");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        detectBeat();
+    }
+}
---
+++ Assets/pulseOnBeat.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pulseOnBeat : MonoBehaviour
+{
+
+    private int colorIndex = 0;
+    private Color[] colorArray =
+        {
+            new Color(255,0,0),
+            new Color(0,255,0),
+            new Color(0,0,255),
+            new Color(127,127,0),
+            new Color(0,127,127),
+            new Color(127,0,127)
+        };
+
+// Start is called before the first frame update
+void Start()
+    {
+
+    }
+
+    void changeCubeColor()
+    {
+
+        if (colorIndex < colorArray.Length - 1)
+        {
+            colorIndex++;
+        }
+        else
+        {
+            colorIndex = 0;
+        }
+       
+        gameObject.GetComponent<Renderer>().material.color = colorArray[colorIndex];
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(BPMFinder.beatFull);
+        if (BPMFinder.beatFull)
+        {
+            Debug.Log("Beat");
+            changeCubeColor();
+        }
+    }
+}
